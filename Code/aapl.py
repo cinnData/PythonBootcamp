455c2aef73cbaecbc0ec49578c555b877fc9dffa
cinnData <canela.miguel@icloud.com>
2023-05-03 17:46:18 +0200

    Add files via upload

---
+++ Code/aapl.py
@@ -0,0 +1,42 @@
+## Example - Apple Inc. stock prices ##
+
+# Q1. Import the data (edit the path) #
+import pandas as pd
+df = pd.read_csv('aapl.csv')
+
+# Q2. Check the contens of the data set #
+df.shape
+df.info()
+df.head()
+df.tail()
+df
+
+# Q3. Data previous to January 15th #
+df[df['date'] < '2022-01-15']
+
+# Q4. Line plot for the opening price #
+df['open'].plot(figsize=(10,6), color='black', linewidth=1);
+
+Q5a. Line plot for the trading volume #
+df['volume'] = df['volume']/10**6
+df['volume'].plot(figsize=(10,6), color='black', linewidth=1);
+
+# Q5b. Histogram for the trading volume #
+df['volume'].plot.hist(figsize=(8,6), color='gray', rwidth=0.98);
+
+# Q6a. Daily variation #
+df['dvar'] = df['high'] - df['low']
+df
+
+# Q6b. Line plot for the daily variation #
+df['dvar'].plot(figsize=(10,6), color='black', linewidth=1);
+
+# Q6c. Histogram for the daily variation #
+df['dvar'].plot.hist(figsize=(8,6), color='gray', rwidth=0.98);
+
+# Q7a. Scatter plot for the daily price variation and the trading volume # 
+df.plot.scatter(x='volume', y='dvar', figsize=(6,6), color='gray');
+
+## Q7. Correlation #
+df['volume'].corr(df['dvar'])
+df['volume'].corr(df['dvar']).round(2)
---
+++ Code/airbnb.py
@@ -0,0 +1,41 @@
+## Barcelona Airbnb listings ##
+
+# Q1. Importing the data #
+import pandas as pd
+path = 'https://raw.githubusercontent.com/cinnData/PythonBootcamp/main/Data/'
+filename = path + 'airbnb.csv'
+df = pd.read_csv(filename, index_col=0)
+df.info()
+df.head()
+
+# Q2a. Counting duplicates #
+df.index.duplicated().sum()
+df.duplicated().sum()
+
+# Q2b. Dropping duplicates #
+df = df.drop_duplicates()
+df.shape
+
+# Q3. How old are the hosts? #
+df['host_id'][df['host_since'] < '2010-01-01'].unique()
+len(df['host_id'].unique())
+df['host_id'][df['host_since'] < '2010-01-01'].value_counts()
+
+# Q4. Proportion of listings with missing ratings #
+df.isna().sum()
+df['review_scores_rating'].isna().mean().round(3)
+
+# Q5. Distribution of the price #
+df['price'].plot.hist(figsize=(8,6), color='gray', rwidth=0.98);
+df['price'].describe()
+df['price'][(df['price'] >= 25) & (df['price'] <= 150)].plot.hist(figsize=(8,6),
+  color='gray', rwidth=0.94, bins=25);
+
+# Q6. Average price per room type #
+df.groupby(by='room_type')['price'].mean().round()
+df.groupby(by='room_type')['price'].median().round()
+
+# Q7. Top-10 neighbourhoods #
+df['neighbourhood'].value_counts().head(10)
+df.groupby(by='neighbourhood')['price'].agg(['count', 'median']).sort_values(by='count',
+  ascending=False).head(10)
