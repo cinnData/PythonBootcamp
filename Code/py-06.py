61b113d1da05bf98203e3f671cad139ab090aa99
cinnData <canela.miguel@icloud.com>
2023-05-11 15:01:02 +0200

    Update py-06.py

--- Code/py-06.py
+++ Code/py-06.py
@@ -4,24 +4,25 @@
 import pandas as pd
 
 # Pandas series #
-s1 = pd.Series(arr1)
+s1 = pd.Series([2, 7, 14, 5, 9])
 s1
 s1.values
 s1.index
 s2 = pd.Series([1, 5, 'Messi'], index = ['a', 'b', 'c'])
 s2
 s2.index
+s2.index[-2:]
 
 # Pandas data frames #
-df = pd.DataFrame({'v1': range(1, 6), 'v2': ['a', 'b', 'c', 'd', 'e'], 'v3': [-1.3, None, 2, 7, 0]})
-df
-df.values
-df.index
-df.columns
-pd.DataFrame(arr2)
+df1 = pd.DataFrame({'v1': range(1, 6), 'v2': ['a', 'b', 'c', 'd', 'e'], 'v3': [-1.3, None, 2, 7, 0]})
+df1
+df1.values
+df1.index
+df1.columns
 import numpy as np
 arr = np.array([range(4), [9, 2.3, 7, 0]])
-pd.DataFrame(arr)
+df2 = pd.DataFrame(arr)
+df2.columns
 
 # Exploring Pandas objects #
 df.head(2)
