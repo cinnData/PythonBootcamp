e115a77c4f3d63ee84f41017b808aa51687f3b8f
cinnData <canela.miguel@icloud.com>
2023-08-21 20:11:33 +0200

    Update py-06.py

--- Code/py-06.py
+++ Code/py-06.py
@@ -1,37 +1,38 @@
-## [PY-06] Introduction to Pandas ##
+## [PY-06] NumPy and Matplotlib ##
 
-# The package Pandas #
-import pandas as pd
-
-# Pandas series #
-s1 = pd.Series([2, 7, 14, 5, 9])
-s1
-s1.values
-s1.index
-s2 = pd.Series([1, 5, 'Messi'], index = ['a', 'b', 'c'])
-s2
-s2.index
-s2.index[-2:]
-
-# Pandas data frames #
-df1 = pd.DataFrame({'v1': range(1, 6), 'v2': ['a', 'b', 'c', 'd', 'e'], 'v3': [-1.3, None, 2, 7, 0]})
-df1
-df1.values
-df1.index
-df1.columns
+# NumPy arrays #
 import numpy as np
-arr = np.array([range(4), [9, 2.3, 7, 0]])
-df2 = pd.DataFrame(arr)
-df2.columns
+arr1 = np.array(range(10))
+arr1
+np.array([2, 'a', True])
+np.array([2, True])
+np.array([2, 3.2])
+arr2 = np.array([[0, 7, 2, 3], [3, 9, -5, 1]])
+arr2
+arr1.shape
+arr2.shape
+arr1[2]
+arr2[1, 2]
 
-# Exploring Pandas objects #
-df.head(2)
-df.info()
-df.describe()
+# NumPy functions #
+np.sqrt(arr1)
+def f(t): return 1/(1 + np.exp(t))
+f(arr2)
 
-# Subsetting data frames #
-df['v2']
-df[['v1', 'v2']]
-df[df['v1'] > 2]
-df['v1'] > 2
-df[df['v1'] > 2][['v1', 'v2']]
+# Subsetting arrays #
+arr1[:3]
+arr2[:1, 1:]
+arr1 > 3
+arr2 > 2
+arr1[arr1 > 3]
+arr2[arr2[:, 0] > 0, :]
+
+# Plotting with Matplotlib #
+import matplotlib.pyplot as plt
+t = np.linspace(0, 2, 100)
+plt.figure(figsize=(6,6))
+plt.title('Figure. Three curves')
+plt.plot(t, t, label='linear', color='black')
+plt.plot(t, t**2, label='quadratic', color='black', linestyle='dashed')
+plt.plot(t, t**3, label='cubic', color='black', linestyle='dotted')
+plt.legend();
