5b9d21cf057e010a6b407a89dee9796b499c0ce0
cinnData <canela.miguel@icloud.com>
2023-10-18 10:52:04 +0200

    Update py-06.py

--- Code/py-06.py
+++ Code/py-06.py
@@ -1,38 +1,23 @@
-## [PY-06] NumPy and Matplotlib ##
+## [PY-06] Programs and modules ##
 
-# NumPy arrays #
-import numpy as np
-arr1 = np.array(range(10))
-arr1
-np.array([2, 'a', True])
-np.array([2, True])
-np.array([2, 3.2])
-arr2 = np.array([[0, 7, 2, 3], [3, 9, -5, 1]])
-arr2
-arr1.shape
-arr2.shape
-arr1[2]
-arr2[1, 2]
+# The working directory #
+%pwd
+%cd py_course
+%pwd
 
-# NumPy functions #
-np.sqrt(arr1)
-def f(t): return 1/(1 + np.exp(t))
-f(arr2)
+# Program 1 #
+print('\nHi, there!\nThis is the Python Course!')
+runfile('program1.py')
 
-# Subsetting arrays #
-arr1[:3]
-arr2[:1, 1:]
-arr1 > 3
-arr2 > 2
-arr1[arr1 > 3]
-arr2[arr2[:, 0] > 0, :]
+# input statements #
+input('\nDo you know Python? (y/n) ')
+int(input('\nWhat is your age? '))
 
-# Plotting with Matplotlib #
-import matplotlib.pyplot as plt
-t = np.linspace(0, 2, 100)
-plt.figure(figsize=(6,6))
-plt.title('Figure. Three curves')
-plt.plot(t, t, label='linear', color='black')
-plt.plot(t, t**2, label='quadratic', color='black', linestyle='dashed')
-plt.plot(t, t**3, label='cubic', color='black', linestyle='dotted')
-plt.legend();
+# Program 2 #
+runfile('program2.py')
+
+# Modules #
+import myfuncs
+myfuncs.has_zero(1, -2, 0)
+from myfuncs import has_zero
+has_zero(1, -2, 3)
