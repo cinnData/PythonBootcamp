dcf00b830367d4a890b613a00720260a3b3ac688
cinnData <canela.miguel@icloud.com>
2023-10-18 10:51:36 +0200

    Update py-05.py

--- Code/py-05.py
+++ Code/py-05.py
@@ -1,31 +1,44 @@
 ## [PY-05] Control statements ##
 
 # If statements #
-if 3 < 5: print('Minor')
-if 3 == 5: print('Equal')
-else: print('Not equal')
-import math
-if math.sqrt(1) < 1: print('Minor')
-elif math.sqrt(1) == 1: print('Equal')
-else: print('Major')
+grade = 9
+if grade >= 5:
+    print('Congratulations!')
+if grade >= 5:
+    print('PASS')
+else:
+	print('FAIL')
+if grade >= 8:
+    print('A')
+elif grade >= 5:
+    print('B')
+else:
+    print('C')
+if grade >= 8:
+    print('Good!')
+elif grade >= 5:
+    print('Keep working!')
 
 # While loops #
 x = 1
-while x**2 <= 1000: x = x + 1
-x
-while x**2 <= 1000: x = x + 1
+while x**2 <= 1000:
+    x = x + 1
 x
 x = 1
-while x > 0: x = x + 1
+while x > 0:
+    x = x + 1
 x
 
 # For loops #
-for i in range(3): print('Hello world!')
+for i in range(3):
+    print('Hello world!')
 squares = []
 for i in range(1, 5):
-	squares = squares + [i**2]
+    squares = squares + [i**2]
 squares
 
 # List comprehensions #
-[i**2 for i in range(1, 5)]
-[i**2 for i in range(1, 21) if i % 3 != 0]
+squares = [i**2 for i in range(1, 5)]
+squares
+my_squares = [i**2 for i in range(1, 21) if i % 3 != 0]
+my_squares
