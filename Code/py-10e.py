73512c5365816a1352d50965231e7a8984c89b02
cinnData <canela.miguel@icloud.com>
2023-10-18 10:57:32 +0200

    Update py-10e.py

--- Code/py-10e.py
+++ Code/py-10e.py
@@ -1,41 +1,43 @@
-## [PY-08E] Example - Apple Inc. stock prices ##
+## [PY-10E] Example - Barcelona Airbnb listings ##
 
-# Import the data (edit the path) #
+# Importing the data #
 import pandas as pd
-%pwd
-df = pd.read_csv('Dropbox/py_course/data/aapl.csv')
+path = 'https://raw.githubusercontent.com/cinnData/PythonBootcamp/main/Data/'
+df = pd.read_csv(path + 'airbnb.csv', index_col=0)
 
 # Exploring the data #
 df.info()
 df.head()
-df.describe()
 
-# Q1. Data previous to January 15th #
-df[df['date'] < '2022-01-15']
+# Q1a. Counting duplicates #
+df.index.duplicated().sum()
+df.duplicated().sum()
 
-# Q2. Line plot for the opening price #
-df['open'].plot(figsize=(10,6), color='black', linewidth=1);
+# Q1b. Dropping duplicates #
+df = df.drop_duplicates()
+df.shape
 
-Q3a. Line plot for the trading volume #
-df['volume'] = df['volume']/10**6
-df['volume'].plot(figsize=(10,6), color='black', linewidth=1);
+# Q2. Proportion of listings with missing ratings #
+df.isna().mean()
+df['review_scores_rating'].isna().mean().round(3)
 
-# Q3b. Histogram for the trading volume #
-df['volume'].plot.hist(figsize=(8,6), color='gray', edgecolor='white');
+# Q3. Distribution of the price #
+df['price'].plot.hist(figsize=(7,5), title='Figure 1. Distribution of the price',
+    color='gray', edgecolor='white', xlabel='Price per night (euros)');
+df['price'].describe()
+filter = df['price'].between(25,175)
+df['price'][filter].plot.hist(figsize=(7,5),
+    title='Figure 2. Distribution of the price (trimmed data)', 
+    color='gray', edgecolor='white', bins=30, xlabel='Price per night (euros)');
 
-# Q4a. Daily variation #
-df['dvar'] = df['high'] - df['low']
-df.head()
+# Q4. Average price per room type #
+pd.pivot_table(df, values='price', index='room_type', aggfunc='mean').round()
+pd.pivot_table(df, values='price', index='room_type', aggfunc='median')
+roomtype_price =  pd.pivot_table(df, values='price', index='room_type', aggfunc='median')
+roomtype_price.plot.bar(figsize=(7,5), legend=False,
+    title='Figure 3. Median price per room type', xlabel='Room type',
+    color='gray', ylabel='Price per night (euros)');
 
-# Q4b. Line plot for the daily variation #
-df['dvar'].plot(figsize=(10,6), color='black', linewidth=1);
-
-# Q4c. Histogram for the daily variation #
-df['dvar'].plot.hist(figsize=(8,6), color='gray', edgecolor='white');
-
-# Q5a. Scatter plot for the daily price variation and the trading volume # 
-df.plot.scatter(x='volume', y='dvar', figsize=(6,6), color='gray');
-
-## Q5b. Correlation #
-df['volume'].corr(df['dvar'])
-df['volume'].corr(df['dvar']).round(2)
+# Q5. Top-10 neighbourhoods #
+df['neighbourhood'].value_counts().head(10)
+df.groupby(by='neighbourhood')['price'].agg(['count', 'median']).sort_values(by='count', ascending=False).head(10)
