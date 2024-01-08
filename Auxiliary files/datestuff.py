d9b057d1a22c8c75a97d2d5343b10e170b25d668
cinnData <canela.miguel@icloud.com>
2024-01-08 16:41:06 +0100

    Delete Auxiliary files/datestuff.py

--- Auxiliary files/datestuff.py
+++
@@ -1,9 +0,0 @@
-month_list = ['January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December']
-
-def convert(date):
-    year = date[:4]
-    month_no = int(date[5:7])
-    month = month_list[month_no - 1]
-    day = str(int(date[8:]))
-    return month + ' ' + day + ', ' + year
